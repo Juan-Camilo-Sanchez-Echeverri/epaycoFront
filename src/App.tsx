4996e9531e1bd28cec442d05532fc3c09145e122
Juan Camilo Sanchez Echeverri <juancamilosanche65@gmail.com>
2024-01-24 08:05:42 -0500

    Update ePayco integration in index.html and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,26 @@
 export default function App() {
-    const data = {
-        //Parametros compra (obligatorio)
-        name: 'Vestido Mujer Primavera',
-        description: 'Vestido Mujer Primavera',
-        currency: 'cop',
-        amount: '12000',
-        tax_base: '0',
-        tax: '0',
-        country: 'co',
-        lang: 'es',
+  const data = {
+    //Parametros compra (obligatorio)
+    amount: 10000,
+    name: 'Vestido Mujer Primavera',
+    description: 'Vestido Mujer Primavera',
+    currency: 'cop',
+    country: 'co',
 
-        //Onpage="false" - Standard="true"
-        external: 'false',
+    //Onpage="false" - Standard="true"
+    external: 'false',
+    // confirmation: 'https://kb43j3v4-3000.use2.devtunnels.ms/api/v1/payments',
+    // response: 'https://kb43j3v4-5500.use2.devtunnels.ms/epayco.html',
+  };
 
-        //Atributos opcionales
-        extra1: 'extra1',
-        extra2: 'extra2',
-        extra3: 'extra3',
-        // confirmation: "http://secure2.payco.co/confirmation.php",
-        //response: "http://secure2.payco.co/response.php",
+  const handler = ePayco.checkout.configure({
+    key: 'aabdfb50003765835312485cfb1622f2',
+    test: true,
+  });
 
-        //Atributos cliente
-        name_billing: 'Andres Perez',
-        address_billing: 'Carrera 19 numero 14 91',
-        type_doc_billing: 'cc',
-        mobilephone_billing: '3050000000',
-        number_doc_billing: '100000000',
-    };
-
-    const handler = ePayco.checkout.configure({
-        key: 'aabdfb50003765835312485cfb1622f2',
-        test: true,
-    });
-
-    return (
-        <div>
-            <button onClick={() => handler.open(data)}>Pagar</button>
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={() => handler.open(data)}>Pagar</button>
+    </div>
+  );
 }
